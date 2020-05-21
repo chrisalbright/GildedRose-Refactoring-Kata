9b07901d2e4f114a4c59b776acebd08077181566
Chris Albright <calbright@cj.com>
2020-05-21 07:54:39 -0700

    Simplify tests

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,6 +1,8 @@
 package com.gildedrose
 
+import com.gildedrose.pricing.GildedRosePricing
 import com.gildedrose.pricing.Pricing
+import com.gildedrose.pricing.Sulfuras
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -23,98 +25,92 @@ class GildedRoseTest {
 
     @Test
     fun `At the end of each day our system lowers both SellIn and Quality for every item`() {
-        val items: Array<Item> = arrayOf(Item("foo", 1, 1))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("foo", app.items[0].name)
-        assertEquals(0, app.items[0].sellIn)
-        assertEquals(0, app.items[0].quality)
+        val item = Item("foo", 1, 1)
+        val updatedItem: Item = GildedRosePricing.update(item)
+        assertEquals("foo", updatedItem.name)
+        assertEquals(0, updatedItem.sellIn)
+        assertEquals(0, updatedItem.quality)
     }
 
     @Test
     fun `Once the sell by date has passed, Quality degrades twice as fast`() {
-        val items: Array<Item> = arrayOf(Item("foo", 1, 9))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(0, app.items[0].sellIn)
-        assertEquals(8, app.items[0].quality)
-        app.updateQuality()
-        assertEquals(6, app.items[0].quality)
+        val item = Item("foo", 1, 9)
+        val updatedItem1 = GildedRosePricing.update(item)
+        assertEquals(0, updatedItem1.sellIn)
+        assertEquals(8, updatedItem1.quality)
+        val updatedItem2 = GildedRosePricing.update(updatedItem1)
+        assertEquals(6, updatedItem2.quality)
     }
 
     @Test
     fun `The Quality of an item is never negative`() {
-        val items: Array<Item> = arrayOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(0, app.items[0].quality)
+        val item = Item("foo", 0, 0)
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(0, newItem.quality)
     }
 
     @Test
     fun `"Aged Brie" actually increases in Quality the older it gets`() {
-        val items: Array<Item> = arrayOf(Item("Aged Brie", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(2, app.items[0].quality)
-
+        val item = Item("Aged Brie", 0, 0)
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(2, newItem.quality)
     }
 
     @Test
     fun `The Quality of an item never becomes more than 50`() {
-        val items: Array<Item> = arrayOf(Item("Aged Brie", 0, 49))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(50, app.items[0].quality)
+        val items = listOf(
+                Item("Aged Brie", 10, 49),
+                Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+        )
+        val updatedItems = items.map { GildedRosePricing.update(it) }
+        updatedItems.forEach {
+            assertEquals(50, it.quality)
+        }
+
     }
 
     @Test
     fun `"Sulfuras", being a legendary item, never has to be sold or decreases in Quality`() {
-        val items: Array<Item> = arrayOf(Item("Sulfuras, Hand of Ragnaros", 0, 20))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(80, app.items[0].quality)
+        val item = Sulfuras
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(Sulfuras.quality, newItem.quality)
     }
 
     @Test
     fun `"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches`() {
-        val items: Array<Item> = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 30, 10))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(11, app.items[0].quality)
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 30, 10)
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(11, newItem.quality)
     }
 
     @Test
     fun `"Backstage passes" Quality increases by 2 when there are 10 days or less`() {
-        val items: Array<Item> = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 10))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(12, app.items[0].quality)
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(12, newItem.quality)
     }
 
     @Test
     fun `"Backstage passes" Quality increases by 3 when there are 5 days or less`() {
-        val items: Array<Item> = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 10))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(13, app.items[0].quality)
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(13, newItem.quality)
     }
 
     @Test
     fun `"Backstage passes" Quality drops to 0 after the concert`() {
-        val items: Array<Item> = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 10))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(0, app.items[0].quality)
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(0, newItem.quality)
     }
 
     @Test
     fun `"Conjured" items degrade in Quality twice as fast as normal items`() {
-        val items: Array<Item> = arrayOf(Item("Conjured Gem", 1, 10))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(8, app.items[0].quality)
-        app.updateQuality()
-        assertEquals(4, app.items[0].quality)
+        val item = Item("Conjured Gem", 1, 10)
+        val newItem = GildedRosePricing.update(item)
+        assertEquals(8, newItem.quality)
+        val newItem2 = GildedRosePricing.update(newItem)
+        assertEquals(4, newItem2.quality)
     }
 }
 
